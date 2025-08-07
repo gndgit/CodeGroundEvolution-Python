cc4937ead2781f4c5d15b0cefa6bb3ce681aaa56
gndgit <gyanendrasb866@gmail.com>
2025-08-07 23:29:16 +0530

    Basic Arthmetic Operation Code

---
+++ Basic_arthmetic.py
@@ -0,0 +1,17 @@
+# Write a Python program to do arithmetical operations addition and division
+
+# Addition Program
+
+print(":Addition, Substraction, Divide, Multiplication, Modulo Operation:\n")
+A = float(input("Enter First Number:\t"))
+B = float(input("Enter Second Number:\t"))
+Addition = A+B
+Substraction = A-B
+Division = A/B
+Multiplication = A*B
+Modulo = A%B 
+print(f"Addition  is {A} + {B} = {Addition} ")
+print(f"Substraction  is {A} - {B} = {Substraction} ")
+print(f"Multiplication  is {A} * {B} = {Multiplication} ")
+print(f"Division  is {A} / {B} = {Division} ")
+print(f"Modulo  is {A} % {B} = {Modulo} ")
