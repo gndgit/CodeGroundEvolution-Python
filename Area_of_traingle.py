33e0d90e02c73715f0a71faaebd0bdfa687399c4
gndgit <gyanendrasb866@gmail.com>
2025-08-07 23:40:46 +0530

    Area of the Triangle

---
+++ Area_of_traingle.py
@@ -0,0 +1,9 @@
+print("\n Question:  Write a Python program to find the area of a triangle?\n")
+
+base = float(input("Enter Length Of the base of triangle:\t"))
+height = float(input("Enter Height Of the triangle:\t"))
+# Calculate The area
+
+area = 0.5 * base * height
+
+print("The Area of the triangle is : ", area)
