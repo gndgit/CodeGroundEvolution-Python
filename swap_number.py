d81108f91386db46345267311b32e4bc735b50e0
gndgit <gyanendrasb866@gmail.com>
2025-08-07 23:50:59 +0530

    Swapping between two Numbers

---
+++ swap_number.py
@@ -0,0 +1,13 @@
+print("\n Question:  Write a Python program to swap two variables?\n")
+
+a = int(input("Enter first (A) number: "))
+b = int(input("Enter second (B) number: "))
+
+temp = a
+a = b
+b = temp
+
+print(f"\nPrevious A Value is {temp},\t Now {a}")
+print(f"\nPrevious B Value is {a},\t Now {temp}")
+
+# Code by gndgit
