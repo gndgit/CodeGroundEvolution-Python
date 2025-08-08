8a7d47d10d472653333e69a2ea0a5cd41896ffab
gndgit <gyanendrasb866@gmail.com>
2025-08-09 01:17:13 +0530

    Adding Some more operations

--- Basic_arthmetic.py
+++ Basic_arthmetic.py
@@ -2,19 +2,26 @@
 
 # Addition Program
 
-print(":Addition, Substraction, Divide, Multiplication, Modulo Operation:\n")
+print(":Addition, Subtraction, Divide, Multiplication, Modulo Operation:\n")
 A = float(input("Enter First Number:\t"))
 B = float(input("Enter Second Number:\t"))
 Addition = A+B
-Substraction = A-B
+Subtraction = A-B
 Division = A/B
+DivisionWhl = A//B
 Multiplication = A*B
 Modulo = A%B 
+ExponentialA = A**2 # A * A
+# if A**5 = A*A*A*A*A
+ExponentialB = B**2
 print(f"Addition  is {A} + {B} = {Addition} ")
-print(f"Substraction  is {A} - {B} = {Substraction} ")
+print(f"Subtraction  is {A} - {B} = {Subtraction} ")
 print(f"Multiplication  is {A} * {B} = {Multiplication} ")
 print(f"Division  is {A} / {B} = {Division} ")
+print(f"Division(Whole Number)  is {A} // {B} = {DivisionWhl} ")
 print(f"Modulo  is {A} % {B} = {Modulo} ")
+print(f"Exponential  is {A} ** 2 = {ExponentialA} ")
+print(f"Exponential  is {B} ** 2 = {ExponentialB} ")
 
 
 # Code by \-\-@gndgit-/-/ :)
