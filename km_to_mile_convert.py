c892bc31919a9623cb8ba0cea97bf8843b31f292
gndgit <gyanendrasb866@gmail.com>
2025-08-09 00:27:33 +0530

    Kilometer to Miles Convertion

---
+++ km_to_mile_convert.py
@@ -0,0 +1,14 @@
+print("\n Python program to convert kilometers to miles.\n")
+
+km = float(input("Enter Kilometer To convert: \t"))
+
+#  1 km = 0.621371
+conv_factor = 0.621371
+
+miles = km * conv_factor
+
+print(f"{km} kilometer is equals to {miles} Miles.")
+
+print("\n -@gndgit- \t")
+
+# Code by \-\-@gndgit-/-/ :)
