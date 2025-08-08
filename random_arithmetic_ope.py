63849e19ce4fbc37fa394c3a89d888e5703533be
gndgit <gyanendrasb866@gmail.com>
2025-08-08 21:24:10 +0530

    Arithmetic Operation Using Random Number Function

---
+++ random_arithmetic_ope.py
@@ -0,0 +1,19 @@
+import random
+
+print("\n Random Number Generator and Arithmetic Operation Making:\n")
+
+A = random.randint(1,99)
+B = random.randint(1,99) # Random Number generation in between 1 to 99
+
+print(f"A Value is {A}")
+print(f"B Value is {B}")
+
+print("\n Arithmetic Operations")
+print(f"{A} + {B} = {A+B}")
+print(f"{A} - {B} = {A-B}")
+print(f"{A} * {B} = {A*B}")
+print(f"{A} / {B} = {A/B}")
+print(f"{A} % {B} = {A%B}")
+
+print("\n -@gndgit- \t")
+# Code by \-\-@gndgit-/-/ :)
