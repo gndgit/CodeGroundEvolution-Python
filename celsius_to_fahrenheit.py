de92a021707fd8c44edefd6421e062b0c2c81a50
gndgit <gyanendrasb866@gmail.com>
2025-08-09 00:24:55 +0530

    Celsius To Fahranheit Converstion

---
+++ celsius_to_fahrenheit.py
@@ -0,0 +1,11 @@
+print("\n Python program to convert Celsius to Fahrenheit.\n")
+
+celsius = float(input("Enter Celsius: "))
+
+# Formula : Fahrenheit = (Celsius * 9/5) + 32
+Fahrenheit = (celsius * 9/5) + 32
+print(f"{celsius} degree celsius is equals to {Fahrenheit} degrees Fahrenheit.")
+
+print("\n -@gndgit- \t")
+
+# Code by \-\-@gndgit-/-/ :)
