bf99d79edac8a58c50c18cfd77c80d65f9664958
gndgit <gyanendrasb866@gmail.com>
2025-08-09 00:26:33 +0530

    Display Full Calendar based on Year and Month

---
+++ display_calendar.py
@@ -0,0 +1,12 @@
+import calendar
+
+print("\n Calendar \n Enter Details to show::")
+
+year = int(input("Enter YEAR: "))
+month = int(input("Enter Month: "))
+
+cal = calendar.month(year,month)
+print(f"\n Calendar of {year} and {month} is \n",cal)
+
+print("\n -@gndgit- \t")
+# Code by \-\-@gndgit-/-/ :)
